f8508ebfe11dab7958792aec8ce12af34cfe645d
mazn91 <mazn.adnan.ma@gmail.com>
2018-03-11 20:53:17 +0300

    task controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mazin
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/tasks', 'TaskController@tasks');
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

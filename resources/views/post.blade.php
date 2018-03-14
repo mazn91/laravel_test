647ef43373e3d82f90f198d0cd0ad678e2d38455
mazn91 <mazn.adnan.ma@gmail.com>
2018-03-15 00:08:49 +0300

    post feature commit

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,5 +5,24 @@
 
 	<h2>Post page!</h2>
 
+	<form action="" method="/post">
+
+		{{ csrf_field() }}
+
+	  <div class="form-group">
+	    <label for="title">Title:</label>
+	    <input type="text" class="form-control" id="title" name="title">
+	  </div>
+
+	  <div class="form-group">
+	    <label for="body">Post:</label>
+	    <textarea name="body" id="body" class="form-control"></textarea>
+	  </div>
+
+
+
+	  <button type="submit" class="btn btn-default">Publish</button>
+	</form>
+
 
 @endsection

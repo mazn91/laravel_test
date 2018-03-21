db34a15a0672ae598e17e22585a712c618a88865
mazn91 <mazn.adnan.ma@gmail.com>
2018-03-21 14:15:50 +0300

    flash meesages commit

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,7 +1,10 @@
 
-$('#flash').delay(500).fadeIn('normal', function() {
-     $(this).delay(2500).fadeOut();
+
+
+$(document).ready(function() {
+ 
+	$('#flash-message').delay(500).fadeIn('normal', function() {
+	     $(this).delay(2500).fadeOut();
+	});
+
 });
-
-
-console.log("works");
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,8 +23,9 @@
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-       <script type="text/javascript" src="{!! asset('js/blog.js') !!}"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        
+        <script type="text/javascript" src="{!! asset('js/blog.js') !!}"></script>
 
   </head>
 
